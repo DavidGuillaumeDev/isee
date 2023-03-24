ae6449b8c8cab7a10fcb2e192c491eddc992d472
Guillaume.D <76114741+DavidGuillaumeDev@users.noreply.github.com>
2023-03-24 02:08:04 +0100

    Modif SearchBar to Animated-SB

--- src/Containers/navBar.js
+++ src/Containers/navBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
 
   return (
     <nav className="fixed top-0 w-full flex items-center justify-between flex-wrap bg-gray-900 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
+      <div className="flex items-center flex-shrink-0 text-white">
         <FiMenu
           className="text-white text-2xl mr-2 cursor-pointer"
           onClick={() => setShowSidebar(!showSidebar)}
@@ -16,17 +16,12 @@ const NavBar = () => {
           iSee
         </Link>
       </div>
-      <div className="flex flex-grow items-center">
-        <div className="flex-grow text-sm">
-          <input
-            className="bg-gray-800 focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full appearance-none leading-normal text-white"
-            type="text"
-            placeholder="Rechercher"
-          />
-        </div>
-        <div className="flex items-center">
-          <FiUser className="text-white text-2xl ml-4" />
-        </div>
+      <div className="src-box">
+        <input type="text" placeholder="  " />
+        <button type="reset"></button>
+      </div>
+      <div className="flex items-center">
+        <FiUser className="text-white text-2xl ml-4" />
       </div>
       <div
         className={`fixed left-0 top-16 bg-gray-900 w-64 p-6 overflow-y-auto transform transition-all duration-300 ease-in-out opacity-90 ${
