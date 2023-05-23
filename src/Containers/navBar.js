25551758ca053eaf15ae2c29e8f2598014d3f345
Guillaume.D <guillaumedavidparis@gmail.com>
2023-05-23 10:14:14 +0200

    Change color Navbar + port 3001

--- src/Containers/navBar.js
+++ src/Containers/navBar.js
@@ -11,10 +11,10 @@ const NavBar = () => {
 
 
   return (
-    <nav className="fixed top-0 w-full flex items-center justify-between flex-wrap bg-gray-900 p-6">
-      <div className="flex items-center flex-shrink-0 text-white">
+    <nav className="fixed top-0 w-full flex items-center bg-white justify-between flex-wrap  p-6">
+      <div className="flex items-center flex-shrink-0 text-balck">
         <FiMenu
-          className="text-white text-3xl mr-2 cursor-pointer"
+          className="text-black text-3xl mr-2 cursor-pointer"
           onClick={() => setShowSidebar(!showSidebar)}
         />
         <Link to="/" className="font-semibold text-xl tracking-tight">
@@ -51,28 +51,28 @@ const NavBar = () => {
         </div>
       </div>
       <div
-        className={`fixed left-0 top-16 bg-gray-900 w-64 p-6 overflow-y-auto transform transition-all duration-300 ease-in-out opacity-90 ${
+        className={`fixed left-0 bg-white top-16 w-64 p-6 overflow-y-auto transform transition-all duration-300 ease-in-out opacity-90 ${
           showSidebar ? "translate-x-0" : "-translate-x-full"
         }`}
         style={{ height: "calc(100vh - 4rem)" }}
       >
           <div className="flex items-center mb-6">
-            <FiHome className="text-white text-xl mr-3" />
-            <Link to="/" className="text-white text-xl">
+            <FiHome className="text-black text-xl mr-3" />
+            <Link to="/" className="text-black text-xl">
               Accueil
             </Link>
           </div> 
           <div className="flex items-center mb-6">
-            <FiTrendingUp className="text-white text-xl mr-3" />
-            <Link to="/tendances" className="text-white text-xl">
+            <FiTrendingUp className="text-black text-xl mr-3" />
+            <Link to="/tendances" className="text-black text-xl">
               Tendances {/* Plus de nb de vues dans les 2 derniers jours */}
             </Link>
           </div>
           {isConnected 
           && 
           <div className="flex items-center mb-6">
-            <FiVideo className="text-white text-xl mr-3" />
-            <Link to="/vos-videos" className="text-white text-xl">
+            <FiVideo className="text-black text-xl mr-3" />
+            <Link to="/vos-videos" className="text-black text-xl">
               Vos Vidéos {/* Liste des vidéos upload par l'utilisateur connecté / Doit s'afficher si un utilisateur est connecté*/}
             </Link>
           </div>
@@ -80,22 +80,22 @@ const NavBar = () => {
           {user.isAdmin 
             && 
             <div className="flex items-center mb-6">
-            <FiBarChart2 className="text-white text-xl mr-3" />
-            <Link to="/dashboard-admin" className="text-white text-xl">
+            <FiBarChart2 className="text-black text-xl mr-3" />
+            <Link to="/dashboard-admin" className="text-black text-xl">
               Tableau de bord admin
             </Link>
           </div>
           } 
           <div className="absolute bottom-0 left-0 mb-6 p-6 w-full">
             <div className="flex items-center mb-6">
-              <FiSettings className="text-white text-xl mr-3" />
-              <Link to="/parametres" className="text-white text-xl">
+              <FiSettings className="text-black text-xl mr-3" />
+              <Link to="/parametres" className="text-black text-xl">
                 Paramètres
               </Link>
             </div>
             <div className="flex items-center">
-              <FiHelpCircle className="text-white text-xl mr-3" />
-              <Link to="/aide-contact" className="text-white text-xl">
+              <FiHelpCircle className="text-black text-xl mr-3" />
+              <Link to="/aide-contact" className="text-black text-xl">
                 Aide/Contact
               </Link>
             </div>
@@ -106,5 +106,6 @@ const NavBar = () => {
 
   );
 };
+
 
 export default NavBar;
